4529ff0c66a63710103053a3f2f2cb300b03f17e
Ryu <x04ty29er@gmail.com>
2011-05-22 13:02:47 -0500

    Added more test strings to the tester

--- testingRevIdx.java
+++ testingRevIdx.java
@@ -27,6 +27,7 @@ public class testingRevIdx {
 		System.out.println("\n\n Adding a LOT more words");
 
 		String thirdInput = "In this paper, we will be exploring the various hypotheses on how people acquire a second or even more, languages. Specifically, we will be looking at the models presented by various scholars that cover both the more nativist approach to language, as well as the more social-linguistic aspects. The main question that we will be addressing is, how do people acquire their non-native languages, and what factors have what kind of influence on this process? We will answer these questions through looking at evidence presented by Schumann, Singleton, and other researchers that have done experiments and conducted research into factor that affect language acquisition, such as age, cultural immersion, and other known languages. We will present data acquired from various studies and attempt to determine how much influence the various factors have, as well as try to determine an order as to the ‘importance’ of each of the variables that has been investigated. As anyone would imagine, there are many factors that play a part in the acquisition of a second or third language, including age and immersion among other things. The question then becomes, how much does each factor play a role in acquisition? And is there a factor that plays more of a part than others? We will be looking at the acculturation model presented by Schumann in 1986, the more sociocognitive approach with alignment presented by Atkinson et al in 2007, as well as the more traditional age based analysis provided by Singleton and Lengyel in their 1995 book.";
+		test.memorize(thirdInput);
 		System.out.println("\n\n\n\n\nTESTING GENERATION:");
 		System.out.println(test.utter(3));
 		System.out.println(test.utter(5));
@@ -38,5 +39,7 @@ public class testingRevIdx {
 		System.out.println(test.utter(17));
 		System.out.println(test.utter(19));
 		System.out.println(test.utter(21));
+		System.out.println("\n\nCurrent Memory");
+		test.testPrint();
 	}
 }
